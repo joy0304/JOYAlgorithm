183ca77983af54540a06d55d117f521dcdb7e87b
Wl201314 <wjl0304@foxmail.com>
2016-05-30 23:39:54 +0800

    swapPairs

--- LeetCode Algorithm/LeetCodeAlgorithm/LeetCodeAlgorithm/Array/array_swapPairs.cpp
+++ LeetCode Algorithm/LeetCodeAlgorithm/LeetCodeAlgorithm/Array/array_swapPairs.cpp
@@ -1,11 +1,3 @@
-//
-//  Test.cpp
-//  LeetCodeAlgorithm
-//
-//  Created by wjl on 16/5/14.
-//  Copyright © 2016年 Martin. All rights reserved.
-//
-
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
@@ -28,7 +20,7 @@ ListNode* swapPairs(ListNode* head) {
     ListNode* next = head->next;
     head->next = swapPairs(next->next);
     next->next = head;
-
+    
     
     return next;
 }
@@ -49,26 +41,4 @@ void ConnectListNodes(ListNode* pCurrent, ListNode* pNext){
     }
     
     pCurrent->next = pNext;
-}
-
-int main(){
- 
-    ListNode *l1 = CreateListNode(1);
-    ListNode *l2 = CreateListNode(2);
-    ListNode *l3 = CreateListNode(3);
-    ListNode *l4 = CreateListNode(4);
-    
-    ConnectListNodes(l1, l2);
-    ConnectListNodes(l2, l3);
-    ConnectListNodes(l3, l4);
-    
-    ListNode *l = swapPairs(l1);
-    
-    ListNode *node = l;
-    
-    while(node != NULL) {
-        printf("%d",node->value);
-        node = node->next;
-    }
-    
 }
